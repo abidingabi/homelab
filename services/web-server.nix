32f5f6b5f57e55d8f2d6e7ae941719384550bdff
Abigail <abigail@dogbuilt.net>
2022-11-24 22:32:05 -0500

    Add static file hosting

--- services/web-server.nix
+++ services/web-server.nix
@@ -1,14 +1,18 @@
-{ pkgs, ... }:
-{
+{ pkgs, ... }: {
   networking.firewall.allowedTCPPorts = [ 80 443 ];
 
   services.caddy = {
     enable = true;
 
     extraConfig = ''
-    signalflags.dogbuilt.net {
-      reverse_proxy localhost:3000
-    }
+      signalflags.dogbuilt.net {
+        reverse_proxy localhost:3000
+      }
+
+      static.dogbuilt.net {
+        root * /public
+        file_server browse
+      }
     '';
   };
 }
